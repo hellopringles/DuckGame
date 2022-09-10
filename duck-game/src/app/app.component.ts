ef007be7aa27132508f4721c478f8e5893610cf1
Darius-Valer Micu <dariusmicu@Darius-Valers-MacBook-Pro.local>
2022-09-10 10:08:12 +0200

    Store score in cookie 🍪

--- duck-game/src/app/app.component.ts
+++ duck-game/src/app/app.component.ts
@@ -30,6 +30,8 @@ export class AppComponent {
     score: number = 0;
 
     ngAfterViewInit() {
+        const savedScore = this.getCookie("SCORE");
+        this.score = savedScore ? parseInt(savedScore) : 0;
         if (this.canvas == null) {
             console.log('null canvas...')
             return;
@@ -79,7 +81,9 @@ export class AppComponent {
             window.requestAnimationFrame(() => this.animate());
             this.score = this.score + ducksPetted;
             this.updateScoreDisplay();
-
+            if (ducksPetted > 0) {
+                this.setCookie("SCORE", this.score.toString())
+            }
         }
     }
 
@@ -127,5 +131,26 @@ export class AppComponent {
             ctx.fillStyle = pixel.color;
             ctx.fill();
         });
+    }
+
+
+    private setCookie(name: string, val: string) {
+        const date = new Date();
+        const value = val;
+    
+        // Set it expire in 7 days
+        date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+    
+        // Set it
+        document.cookie = name+"="+value+"; expires="+date.toUTCString()+"; path=/";
+    }
+    
+    private getCookie(name: string) {
+        const value = "; " + document.cookie;
+        const parts = value.split("; " + name + "=");
+        
+        if (parts.length == 2) {
+            return (parts.pop() as any).split(";").shift();
+        }
     }
 }
